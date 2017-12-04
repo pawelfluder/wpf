e113ed85e93ca65765565524f16f4da3777729b1
Paweł Fluder <pawel.fluder@pirios.com>
2017-12-04 13:36:50 +0100

    (3) Użycie ICommand z parametrem dla checkbox'ów z listy Popup'a

---
+++ 01_CheckBoxComboBox/CheckBoxComboBoxApp/CheckBoxComboBoxApp/PiriosUserControl/CbCbBox/CheckBoxComboBoxViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows.Input;
+
+namespace CheckBoxComboBoxApp.PiriosUserControl.CbCbBox
+{
+    class CheckBoxComboBoxViewModel
+    {
+        public ICommand btnClickCmd;
+
+        public ICommand BtnClickCmd
+        {
+            get
+            {
+                if (btnClickCmd == null)
+                    btnClickCmd = new RelayCommand<object>(o => CheckBox_Click(o));
+                return btnClickCmd;
+            }
+        }
+
+        private void SetBtnClickCmd()
+        {
+            btnClickCmd = new RelayCommand<object>(o => CheckBox_Click(o));
+        }
+
+        private void CheckBox_Click(object obj)
+        {
+        }
+    }
+}
---
+++ 01_CheckBoxComboBox/CheckBoxComboBoxApp/CheckBoxComboBoxApp/PiriosUserControl/RelayCommand.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace CheckBoxComboBoxApp.PiriosUserControl
+{
+    public class RelayCommand<T> : ICommand
+    {
+        private readonly Action<T> _execute;
+        private readonly Predicate<T> _canExecute;
+
+        public RelayCommand(Action<T> execute)
+            : this(execute, (Predicate<T>)null)
+        {
+        }
+
+        public RelayCommand(Action<T> execute, Predicate<T> canExecute)
+        {
+            if (execute == null)
+                throw new ArgumentNullException(nameof(execute));
+            this._execute = execute;
+            this._canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                if (this._canExecute == null)
+                    return;
+                CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                if (this._canExecute == null)
+                    return;
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CommandManager.InvalidateRequerySuggested();
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return this._canExecute == null || this._canExecute((T)parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            this._execute((T)parameter);
+        }
+    }
+}
