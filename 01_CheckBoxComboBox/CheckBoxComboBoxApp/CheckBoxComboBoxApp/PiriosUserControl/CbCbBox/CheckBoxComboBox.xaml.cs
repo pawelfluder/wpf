f8a5cf454667d23f4faa5660001d5086a7692f6d
Paweł Fluder <pawel.fluder@pirios.com>
2017-12-04 13:20:38 +0100

    (1) Zbindowanie IsChecked ToggleButton'a z IsOpen Popup'a.

---
+++ 01_CheckBoxComboBox/CheckBoxComboBoxApp/CheckBoxComboBoxApp/PiriosUserControl/CbCbBox/CheckBoxComboBox.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace CheckBoxComboBoxApp.PiriosUserControl.CbCbBox
+{
+    public partial class CheckBoxComboBox : UserControl
+    {
+        public CheckBoxComboBox()
+        {
+            InitializeComponent();
+        }
+    }
+}
